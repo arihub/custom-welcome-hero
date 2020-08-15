99168404ba0330b31da41f7746bb738f4afad37a
Daniel <6577746+funkeye@users.noreply.github.com>
2020-08-15 23:53:06 +0200

    bugfix

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,9 +1,9 @@
 import { override } from "flarum/extend";
-import IndexPage from "flarum/components/IndexPage";
-import CustomWelcomeHero from "./components/CustomWelcomeHero";
 
+import WelcomeHero from "flarum/components/WelcomeHero";
+import CustomWelcomeHero from "./components/CustomWelcomeHero";
 app.initializers.add("funkeye-custom-welcome-hero", () => {
-  override(IndexPage.prototype, "hero", (el) => {
+  override(WelcomeHero.prototype, "view", (el) => {
     return CustomWelcomeHero.component();
   });
 });
